8dac70379a69f49aeff74e243c531c6d1170b59d
mj52951 <mj52951@fer.hr>
2023-07-13 13:24:19 +0200

    Created program action

--- src/bridgeDebugging.ts
+++ src/bridgeDebugging.ts
@@ -1,11 +1,10 @@
 import 'dotenv/config';
 import axios from 'axios';
-import { ethers } from 'ethers';
-import { Command, Option } from 'commander';
+import { Command } from 'commander';
 import { SharedConfig } from "./constants";
-import { getWalletsForDifferentProviders, deriveWalletsFromMnemonic, sendPauseTransactions } from "./utils";
 import { RawConfig, Domain } from '@buildwithsygma/sygma-sdk-core';
-import { config } from 'process';
+import { getTransactionInfo } from 'utils';
+
 
 
 const program = new Command();
@@ -13,14 +12,33 @@ const program = new Command();
 program
     .name("debug-bridge")
     .description("Enables quick debugging of specific Sygma messages (bridging request)")
-    .addOption(
-        new Option("-e, --environment <environment>", "Environment on which to debug")
-            .choices(["devnet", "mainnet", "testnet"])
-    )
-    .addOption(
-        new Option("-d, --deposit", "Deposit transaction on which to provide status")
+    .requiredOption(
+        "-e, --environment <environment>", "Environment on which to debug"
+    ).
+    requiredOption(
+        "-d, --deposit <depositHash>", "Deposit transaction on which to provide status"
     ).action(async(configs: any) => {
+        try {
+            const network: keyof typeof SharedConfig = configs.environment; 
+            const depositHash: string = configs.depositHash;
+            
+            const {
+                data
+            } = await axios.get(SharedConfig[network]) as unknown as { data: RawConfig };
 
+            const networks: Array<Domain> = data.domains.filter((network: Domain) => network.name === "ethereum"); // just evms for now
+
+            await getTransactionInfo(networks, depositHash)
+            
+
+
+        } catch (err) {
+            if (err instanceof Error) {
+                throw new Error(`Failed to fetch shared config because of: ${err.message}`);
+              } else {
+                throw new Error('Something went wrong while fetching config file');
+              }
+        }
     })
 
 
