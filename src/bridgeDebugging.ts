0124a4999cf225b7ae86b5d8277fb47e1d041c5b
mj52951 <mj52951@fer.hr>
2023-07-13 14:01:39 +0200

    Added new option for specifying chainId

--- src/bridgeDebugging.ts
+++ src/bridgeDebugging.ts
@@ -3,9 +3,7 @@ import axios from 'axios';
 import { Command } from 'commander';
 import { SharedConfig } from "./constants";
 import { RawConfig, Domain } from '@buildwithsygma/sygma-sdk-core';
-import { getTransactionInfo } from 'utils';
-
-
+import { getTransactionInfo } from './utils';
 
 const program = new Command();
 
@@ -16,8 +14,12 @@ program
         "-e, --environment <environment>", "Environment on which to debug"
     ).
     requiredOption(
-        "-d, --deposit <depositHash>", "Deposit transaction on which to provide status"
-    ).action(async(configs: any) => {
+        "-d, --deposit <depositHash>", "Deposit transaction on which to provide information"
+    ).
+    requiredOption(
+        "-cid, --chain-id <chainId>", "Chain on which the transaction happened"
+    )
+    .action(async(configs: any) => {
         try {
             const network: keyof typeof SharedConfig = configs.environment; 
             const depositHash: string = configs.depositHash;
@@ -25,12 +27,9 @@ program
             const {
                 data
             } = await axios.get(SharedConfig[network]) as unknown as { data: RawConfig };
+            const domain: any = data.domains.filter((domain: Domain) => domain.name === "ethereum" && domain.chainId === configs.chainId); // just evms for now
 
-            const networks: Array<Domain> = data.domains.filter((network: Domain) => network.name === "ethereum"); // just evms for now
-
-            await getTransactionInfo(networks, depositHash)
-            
-
+            await getTransactionInfo(domain, depositHash)
 
         } catch (err) {
             if (err instanceof Error) {
