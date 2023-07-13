f8de08e9bea26b76c66cce3f3cb6366f84f4a308
mj52951 <mj52951@fer.hr>
2023-07-13 16:22:58 +0200

    Only chainId is required for transaction info

--- src/bridgeDebugging.ts
+++ src/bridgeDebugging.ts
@@ -14,7 +14,7 @@ program
         "-e, --environment <environment>", "Environment on which to debug"
     ).
     requiredOption(
-        "-d, --deposit <depositHash>", "Deposit transaction on which to provide information"
+        "-d, --deposit <deposit>", "Deposit transaction on which to provide information"
     ).
     requiredOption(
         "-cid, --chain-id <chainId>", "Chain on which the transaction happened"
@@ -22,20 +22,23 @@ program
     .action(async(configs: any) => {
         try {
             const network: keyof typeof SharedConfig = configs.environment; 
-            const depositHash: string = configs.depositHash;
-            
             const {
                 data
             } = await axios.get(SharedConfig[network]) as unknown as { data: RawConfig };
-            const domain: any = data.domains.filter((domain: Domain) => domain.name === "ethereum" && domain.chainId === configs.chainId); // just evms for now
 
-            await getTransactionInfo(domain, depositHash)
+            if (!data.domains.filter((domain: Domain) => 
+                    domain.name === "ethereum" 
+                    && domain.chainId == configs.chainId) // just evms for now
+            ){
+                throw new Error("Chain doesn't exist")
+            }
+            await getTransactionInfo(configs.deposit, configs.chainId)
 
         } catch (err) {
             if (err instanceof Error) {
-                throw new Error(`Failed to fetch shared config because of: ${err.message}`);
+                throw new Error(`Failed to execute because of: ${err.message}`);
               } else {
-                throw new Error('Something went wrong while fetching config file');
+                throw new Error('Something went wrong');
               }
         }
     })
