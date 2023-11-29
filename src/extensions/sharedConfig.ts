70b730d77af47252c85af1f3d3969be4ea0ee447
tcar <tcar121293@gmail.com>
2023-11-29 13:14:39 +0100

    use local sharedConfig only if defined;
    Signed-off-by: tcar <tcar121293@gmail.com>

--- src/extensions/sharedConfig.ts
+++ src/extensions/sharedConfig.ts
@@ -14,31 +14,32 @@ module.exports = (toolbox: GluegunToolbox) => {
       choices: Object.keys(Environment),
     })
 
-    // if local environment selected, configure shared-config through sharedConfig.json
-    if(result.env.toLowerCase() == Environment.LOCAL) {
+    // if domains are configured in local sharedConfig.json file, use local sharedConfig data
       toolbox.env = result.env
-      return filesystem.read(
+      const rawConfig =  filesystem.read(
         'sharedConfig.json',
         'json'
       ) as RawConfig
-    }
 
-    const api = http.create({
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-      baseURL: ConfigUrl[result.env],
-    })
-    ConfigUrl[result.env]
-    /**
-     * This is an empty string because we don't have a unique base url
-     * across all environments. To avoid excess logic
-     * the whole shared config url is passed as base url.
-     */
-    const { ok, data } = await api.get('')
-    if (!ok) {
-      throw new Error('Failed to fetch shared config.')
+    if(rawConfig.domains.length) {
+      return rawConfig
+    } else {
+      const api = http.create({
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        baseURL: ConfigUrl[result.env],
+      })
+      ConfigUrl[result.env]
+      /**
+       * This is an empty string because we don't have a unique base url
+       * across all environments. To avoid excess logic
+       * the whole shared config url is passed as base url.
+       */
+      const { ok, data } = await api.get('')
+      if (!ok) {
+        throw new Error('Failed to fetch shared config.')
+      }
+      return data as RawConfig
     }
-    toolbox.env = result.env
-    return data as RawConfig
   }
   toolbox.sharedConfig = { fetchSharedConfig }
 }
